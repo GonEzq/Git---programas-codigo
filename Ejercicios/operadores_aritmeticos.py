e9a6dd08a2a667b23977772998e77ee2cce8fd93
GonEzq <gonzaloeriveral@gmail.com>
2023-06-11 15:07:34 -0300

    ejercicios11-06

---
+++ Ejercicios/operadores_aritmeticos.py
@@ -0,0 +1,32 @@
+#suma y resta (+y-)
+
+suma = 12 + 5
+resta = 12 - 5
+
+#multi y division (* y /)
+
+multi = 12 * 5
+division = 12 / 5 #devuelve un dato float (flotante)
+
+#potenciacion (**)
+expotente =12 ** 5
+
+#division baja (//)
+division_baja = 12 // 5  #devuelve un entero redondeando hacia abajo
+
+#resto o modulo
+resto = 12 % 5
+
+print (suma)
+
+print (resta)
+
+print (multi)
+
+print (division)
+
+print (expotente)
+
+print (division_baja)
+
+print (resto)
---
+++ Ejercicios/operadores_logicos.py
@@ -0,0 +1,33 @@
+#operadores logicos
+
+#AND &
+
+Resultado1 = True & True #Devolver True
+Resultado2 = False & True #Devolver Falso
+Resultado3 = True & False #Devolver Falso
+Resultado4 = False & False #Devolver Falso
+
+#OR |
+
+Resultado5 = True | True #Devolver True
+Resultado6 = False | True #Devolver True
+Resultado7 = True | False #Devolver True
+Resultado8 = False | False #Devolver Falso
+
+#NOT 
+
+Resultado9 = not True #Devolver Falso
+Resultado10 = not False #Devolver True
+
+print (Resultado1)
+print (Resultado2)
+print (Resultado3)
+print (Resultado4)
+
+print (Resultado5)
+print (Resultado6)
+print (Resultado7)
+print (Resultado8)
+
+print (Resultado9)
+print (Resultado10)
