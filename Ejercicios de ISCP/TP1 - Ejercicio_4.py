3eb401f1458b9f59d6a27bd460f8e431798ca90c
GonEzq <gonzaloeriveral@gmail.com>
2023-06-11 17:16:32 -0300

    17:16

--- Ejercicios de ISCP/TP1 - Ejercicio_4.py
+++ Ejercicios de ISCP/TP1 - Ejercicio_4.py
@@ -10,18 +10,3 @@ sumatoria_positivos = sum(numeros_positivos)
 print("Números positivos:", numeros_positivos)
 print("Sumatoria de los números positivos:", sumatoria_positivos)
 
-#5. Leer 15 números negativos y convertirlos a positivos e imprimir dichos
-#números.
-
-numeros_negativos = []
-for i in range(15):
-    numero = int(input("Ingrese un número negativo: "))
-    if numero < 0:
-        numeros_negativos.append(numero)
-    else:
-        print("Este número no es negativo. Ingrese uno negativo.")
-        i -= 1
-
-numeros_positivos = [-numero for numero in numeros_negativos]
-print("Números positivos correspondientes:")
-print(numeros_positivos)
